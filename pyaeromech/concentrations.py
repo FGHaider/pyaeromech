5901d3b94cb8f9ed142dee774da168ebef3b51d2
FGHaider <fredrikhaider@outlook.com>
2025-03-05 21:53:34 +0100

    Reformatted some of the concentration factors to be simple functions rather than classes.

--- pyaeromech/concentrations.py
+++ pyaeromech/concentrations.py
@@ -2,191 +2,156 @@
 import numpy as np
 from pyaeromech.exceptions import OutsideRange
 
+    
+def Kt_calc(h, r, D, C):
+    ratio = h/r
+    coefficients = C[:,0] + C[:,1]*np.sqrt(ratio) + C[:,2]*ratio
+    C1 = coefficients[0]
+    C2 = coefficients[1]
+    C3 = coefficients[2]
+    C4 = coefficients[3]
+    ratio = 2*h/D
+    return C1 + C2*ratio + C3*ratio**2 + C4*ratio**3
+        
+def SquareShoulderFilletRectangularSection(L, D, r, h, loadtype):
+    ratio = h/r
+    if L/D < 3/((r/(D-2*h))**(1/4)):
+        raise("The ratio L/D is outside the valid limits")
+    
+    if loadtype == "axial_tension":
+        if 0.1 <= ratio <= 2.0:
+            C = np.array([[1.007, 1.000, -0.031],
+                            [-0.114, -0.585, 0.314],
+                            [0.241, -0.992, -0.271],
+                            [-0.134, 0.577, -0.012]])
+        elif 2.0 <= ratio <= 20.0:
+            C = np.array([[1.042, 0.982, -0.036],
+                            [-0.074, -0.156, -0.010],
+                            [-3.418, 1.220, -0.005],
+                            [3.450, -2.046, 0.051]])
+    
+    elif loadtype == "in_plane_bending":
+        if 0.1 <= ratio <= 2.0:
+            C = np.array([[1.007, 1.000, -0.031],
+                            [-0.270, -2.404, 0.749],
+                            [0.677, 1.133, -0.904],
+                            [-0.414, 0.271, 0.186]])
+        elif 2.0 <= ratio <= 20.0:
+            C = np.array([[1.042, 0.982, -0.036],
+                            [-3.599, 1.619, -0.431],
+                            [6.084, -5.607, 1.158],
+                            [-2.527, 3.006, -0.691]])
+            
+    else:
+        raise(OutsideRange("The ratio h/r is outside the valid limits"))
+    
+    return Kt_calc(h, r, D, C)
+        
+    
+def TwoUNotchesRectangularSection(h, r, D, loadtype):
 
-class StressConcentration:
+    ratio = h/r
     
-    def Kt(self, h, r, D, C):
-        ratio = h/r
-        coefficients = C[:,0] + C[:,1]*np.sqrt(ratio) + C[:,2]*ratio
-        C1 = coefficients[0]
-        C2 = coefficients[1]
-        C3 = coefficients[2]
-        C4 = coefficients[3]
-        ratio = 2*h/D
-        return C1 + C2*ratio + C3*ratio**2 + C4*ratio**3
+    if loadtype == "axial_tension":
+        if ratio == 1:
+            C = np.array([3.065, -3.370, 0.647, 0.658])
+        elif 0.25 <= ratio <= 2.0:
+            C = np.array([[0.850, 2.628, -0.413],
+                            [-1.119, -4.826, 2.575],
+                            [3.563, -0.514, -2.402],
+                            [-2.294, 2.713, 0.240]])
+        elif 2.0 <= ratio <= 50.0:
+            C = np.array([[0.833, 2.069, -0.009],
+                            [2.732, -4.157, 0.176],
+                            [-8.859, 5.327, -0.320],
+                            [6.294, -3.239, 0.154]])
+    
+    elif loadtype == "in_plane_bending":
+        if ratio == 1:
+            C = np.array([3.065, -6.269, 7.015, -2.812])
+        elif 0.25 <= ratio <= 2.0:
+            C = np.array([[0.723, 2.845, -0.504],
+                            [-1.836, -5.746, 1.314],
+                            [7.254, -1.885, 1.646],
+                            [-5.140, 4.785, -2.456]])
+        elif 2.0 <= ratio <= 50.0:
+            C = np.array([[0.833, 2.069, -0.009],
+                            [0.024, -5.383, 0.126],
+                            [-0.856, 6.460, -0.199],
+                            [0.999, -3.146, 0.082]])
+    
+    elif loadtype == "out_of_plane_bending":
+        if ratio == 1:
+            C = np.array([1.876, -2.756, 3.056, -1.175])
+        elif 0.25 <= ratio <= 4.0:
+            C = np.array([[1.031, 0.831, 0.014],
+                            [-1.227, -1.646, 0.117],
+                            [3.337, -0.750, 0.469],
+                            [-2.141, 1.566, -0.600]])
+            
+    else:
+        raise(OutsideRange("The ratio h/r is outside the valid limits"))
+    
 
-class SquareShoulderFilletRectangularSection(StressConcentration):
-    
-    def __init__(self, h, r, D, L, loadtype) -> None:
-        super().__init__()
-        self.r = r
-        self.h = h
-        self.D = D
-        self.loadtype = loadtype
-        
-    def factors(self, L, D, r, h):
-        ratio = h/r
-        if L/D < 3/((r/(D-2*h))**(1/4)):
-            raise("The ratio L/D is outside the valid limits")
-        
-        if self.loadtype == "axial_tension":
-            if 0.1 <= ratio <= 2.0:
-                self.C = np.array([[1.007, 1.000, -0.031],
-                                [-0.114, -0.585, 0.314],
-                                [0.241, -0.992, -0.271],
-                                [-0.134, 0.577, -0.012]])
-            elif 2.0 <= ratio <= 20.0:
-                self.C = np.array([[1.042, 0.982, -0.036],
-                                [-0.074, -0.156, -0.010],
-                                [-3.418, 1.220, -0.005],
-                                [3.450, -2.046, 0.051]])
-        
-        elif self.loadtype == "in_plane_bending":
-            if 0.1 <= ratio <= 2.0:
-                self.C = np.array([[1.007, 1.000, -0.031],
-                                [-0.270, -2.404, 0.749],
-                                [0.677, 1.133, -0.904],
-                                [-0.414, 0.271, 0.186]])
-            elif 2.0 <= ratio <= 20.0:
-                self.C = np.array([[1.042, 0.982, -0.036],
-                                [-3.599, 1.619, -0.431],
-                                [6.084, -5.607, 1.158],
-                                [-2.527, 3.006, -0.691]])
-                
-        else:
-            raise(OutsideRange("The ratio h/r is outside the valid limits"))
-        
+    return Kt_calc(h, r, D, C)
 
-class TwoUNotchesRectangularSection(StressConcentration):
     
-    def __init__(self, h, r, D, loadtype) -> None:
-        super().__init__()
-        self.r = r
-        self.h = h
-        self.D = D
-        self.loadtype = loadtype
-        self.combinations = np.meshgrid(r, h, D)
-        results = np.empty([4, len(np.matrix.flatten(self.combinations[0]))])
+def SingleUNotchRectangularSection(h, r, D, loadtype):
+    ratio = h/r
+    
+    if loadtype == "axial_tension":
+        if ratio == 1:
+            C = np.array([2.988, -7.300, 9.742, -4.429])
+        elif 0.5 <= ratio <= 4.0:
+            C = np.array([[0.721, 2.394, -0.127],
+                            [1.978, -11.489, 2.211],
+                            [-4.413, 18.751, -4.596],
+                            [2.714, -9.655, 2.512]])
+    
+    elif loadtype == "in_plane_bending":
+        if ratio == 1:
+            C = np.array([2.988, -7.735, 10.674, -4.927])
+        elif 0.5 <= ratio <= 4.0:
+            C = np.array([[0.721, 2.394, -0.127],
+                            [-0.426, -8.827, 1.518],
+                            [2.161, 10.968, -2.455],
+                            [-1.456, -4.535, 1.064]])
+            
+    else:
+        raise(OutsideRange("The ratio h/r is outside the valid limits"))
+    
+    return Kt_calc(h, r, D, C)
+        
+    
+def SingleVNotchRectangularSection(h, r, D, theta):
+    ratio = 2*h/D
+    Ktu = SingleUNotchRectangularSection(h,r,D, loadtype="in_plane_bending")
+    
+    if theta <= 150:
+        KtTheta = 1.11*Ktu - (0.0275 + 0.1125 * (theta/150)**4)*Ktu**2
+    else:
+        KtTheta = 1e3
+    
+    Kt = min(Ktu, KtTheta)
+    
+    return Kt
+        
+    
+def TwoVNotchesRectangularSection(h, r, D, theta) -> None:
 
-        for idx, (ri, hi, Di) in enumerate(zip(np.matrix.flatten(self.combinations[0]), np.matrix.flatten(self.combinations[1]), np.matrix.flatten(self.combinations[2]))):
-            C = self.factors(ri, hi)
-            Kti = self.Kt(hi, ri, Di, C)
-            results[0, idx] = ri
-            results[1, idx] = hi
-            results[2, idx] = Di
-            results[3, idx] = Kti
+    ratio = 2*h/D
+    Ktu = TwoUNotchesRectangularSection(h,r,D, loadtype="axial_tension")
     
-    def factors(self, r, h):
-        ratio = h/r
-        
-        if self.loadtype == "axial_tension":
-            if ratio == 1:
-                C = np.array([3.065, -3.370, 0.647, 0.658])
-            elif 0.25 <= ratio <= 2.0:
-                C = np.array([[0.850, 2.628, -0.413],
-                                [-1.119, -4.826, 2.575],
-                                [3.563, -0.514, -2.402],
-                                [-2.294, 2.713, 0.240]])
-            elif 2.0 <= ratio <= 50.0:
-                C = np.array([[0.833, 2.069, -0.009],
-                                [2.732, -4.157, 0.176],
-                                [-8.859, 5.327, -0.320],
-                                [6.294, -3.239, 0.154]])
-        
-        elif self.loadtype == "in_plane_bending":
-            if ratio == 1:
-                C = np.array([3.065, -6.269, 7.015, -2.812])
-            elif 0.25 <= ratio <= 2.0:
-                C = np.array([[0.723, 2.845, -0.504],
-                                [-1.836, -5.746, 1.314],
-                                [7.254, -1.885, 1.646],
-                                [-5.140, 4.785, -2.456]])
-            elif 2.0 <= ratio <= 50.0:
-                C = np.array([[0.833, 2.069, -0.009],
-                                [0.024, -5.383, 0.126],
-                                [-0.856, 6.460, -0.199],
-                                [0.999, -3.146, 0.082]])
-        
-        elif self.loadtype == "out_of_plane_bending":
-            if ratio == 1:
-                C = np.array([1.876, -2.756, 3.056, -1.175])
-            elif 0.25 <= ratio <= 4.0:
-                C = np.array([[1.031, 0.831, 0.014],
-                                [-1.227, -1.646, 0.117],
-                                [3.337, -0.750, 0.469],
-                                [-2.141, 1.566, -0.600]])
-                
-        else:
-            raise(OutsideRange("The ratio h/r is outside the valid limits"))
-        
-        return C
-
-class SingleUNotchRectangularSection(StressConcentration):
+    if ratio == 0.4 and theta <= 120:
+        KtTheta = 1.11*Ktu - (0.0275 + 0.000145*theta + 0.00164 * (theta/120)**8)*Ktu**2
+    elif ratio == 0.667 and theta <= 120:
+        KtTheta = 1.11*Ktu - (0.0275 + 0.00042*theta + 0.0075 * (theta/120)**8)*Ktu**2
+    else:
+        KtTheta = 1e3
     
-    def __init__(self, h, r, D, loadtype) -> None:
-        super().__init__()
-        self.r = r
-        self.h = h
-        self.D = D
-        ratio = h/r
+    Kt = min(Ktu, KtTheta)
+    return Kt
         
-        if loadtype == "axial_tension":
-            if ratio == 1:
-                self.C = np.array([2.988, -7.300, 9.742, -4.429])
-            elif 0.5 <= ratio <= 4.0:
-                self.C = np.array([[0.721, 2.394, -0.127],
-                                [1.978, -11.489, 2.211],
-                                [-4.413, 18.751, -4.596],
-                                [2.714, -9.655, 2.512]])
-        
-        elif loadtype == "in_plane_bending":
-            if ratio == 1:
-                self.C = np.array([2.988, -7.735, 10.674, -4.927])
-            elif 0.5 <= ratio <= 4.0:
-                self.C = np.array([[0.721, 2.394, -0.127],
-                                [-0.426, -8.827, 1.518],
-                                [2.161, 10.968, -2.455],
-                                [-1.456, -4.535, 1.064]])
-                
-        else:
-            raise(OutsideRange("The ratio h/r is outside the valid limits"))
-        
-class SingleVNotchRectangularSection(StressConcentration):
-    
-    def __init__(self, h, r, D, theta) -> None:
-        self.r = r
-        self.h = h
-        self.D = D
-        ratio = 2*h/D
-        u_notch_reference = SingleUNotchRectangularSection(h,r,D, loadtype="in_plane_bending")
-        
-        Ktu = u_notch_reference.Kt()
-        if theta <= 150:
-            KtTheta = 1.11*Ktu - (0.0275 + 0.1125 * (theta/150)**4)*Ktu**2
-        else:
-            KtTheta = 1e3
-        
-        Kt = min(Ktu, KtTheta)
-
-class TwoVNotchesRectangularSection(StressConcentration):
-    
-    def __init__(self, h, r, D, theta) -> None:
-        self.r = r
-        self.h = h
-        self.D = D
-        
-        ratio = 2*h/D
-        u_notch_reference = TwoUNotchesRectangularSection(h,r,D, loadtype="axial_tension")
-        
-        Ktu = u_notch_reference.Kt()
-        if ratio == 0.4 and theta <= 120:
-            KtTheta = 1.11*Ktu - (0.0275 + 0.000145*theta + 0.00164 * (theta/120)**8)*Ktu**2
-        elif ratio == 0.667 and theta <= 120:
-            KtTheta = 1.11*Ktu - (0.0275 + 0.00042*theta + 0.0075 * (theta/120)**8)*Ktu**2
-        else:
-            KtTheta = 1e3
-        
-        Kt = min(Ktu, KtTheta)
 
 def sigma_nom(M, t, d):
 
@@ -226,6 +191,19 @@ def rectangular_bar_with_notches(w, h, r): # Does not seem to align with Roarks,
 
 
 def rectangular_single_notch_bending(h, D, r):
+    """_summary_
+
+    Args:
+        h (_type_): _description_
+        D (_type_): _description_
+        r (_type_): _description_
+
+    Raises:
+        OutsideRange: _description_
+
+    Returns:
+        _type_: _description_
+    """
     a_set = h/r
     Kt_set = np.empty(a_set.shape)
 
